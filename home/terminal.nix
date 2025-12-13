b0a83546d906fdfe970d12f644e3ed66e01d11dd
bl4dee <131567585+bl4dee@users.noreply.github.com>
2025-12-12 16:18:05 -0800

    eza theme

--- home/terminal.nix
+++ home/terminal.nix
@@ -196,6 +196,17 @@
     icons = "auto";
   };
 
+  xdg.configFile."eza/theme.yml".source =
+    let
+      eza-themes = pkgs.fetchFromGitHub {
+        owner = "eza-community";
+        repo = "eza-themes";
+        rev = "c03051f67e84110fbae91ab7cbc377b3460f035c";
+        hash = "sha256-qEC7H9/ghnjkwmMZ788TSgS9ysyIfD+3NHCjxq0Dps0=";
+      };
+    in
+    "${eza-themes}/themes/catppuccin.yml";
+
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
@@ -240,7 +251,6 @@
     settings = {
       theme = "catppuccin-mocha";
       default_layout = "compact";
-      copy_on_select = false;
       show_startup_tips = false;
     };
   };
