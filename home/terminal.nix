727c9959f92d64326c084c4fd5fe6941be390acd
bl4dee <131567585+bl4dee@users.noreply.github.com>
2025-12-20 17:56:05 -0800

    Set atuin to accept on enter and fix wez not copying

--- home/terminal.nix
+++ home/terminal.nix
@@ -225,6 +225,7 @@
     settings = {
       auto_sync = false;
       update_check = false;
+      enter_accept = true;
     };
   };
 
@@ -333,7 +334,16 @@
         enable_tab_bar = false,
         disable_default_key_bindings = true,
         keys = {
-          { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
+          {
+            key = "c",
+            mods = "CTRL|SHIFT",
+            action = wezterm.action_callback(function(window, pane)
+              local sel = window:get_selection_text_for_pane(pane)
+              if sel and sel ~= "" then
+                window:perform_action(wezterm.action.CopyTo("Clipboard"), pane)
+              end
+            end)
+          },
           { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
         },
         window_padding = {
