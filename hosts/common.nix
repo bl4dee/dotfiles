31cbc0b11903ffdfd4c63cc2128efd7e3d986385
bl4dee <131567585+bl4dee@users.noreply.github.com>
2026-01-22 01:09:06 -0800

    doh peak

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # nix
@@ -6,6 +6,7 @@
     "nix-command"
     "flakes"
   ];
+  nix.settings.warn-dirty = false;
   nixpkgs.config.allowUnfree = true;
   programs.nix-ld.enable = true;
   system.stateVersion = "24.11";
@@ -28,6 +29,8 @@
 
   # networking (hostName is host-specific)
   networking.networkmanager.enable = true;
+  networking.networkmanager.dns = lib.mkForce "none";
+  networking.nameservers = [ "127.0.0.1" "::1" ];
 
   # locale
   time.timeZone = "America/Los_Angeles";
@@ -129,6 +132,40 @@
   services.tor = {
     enable = true;
     client.enable = true;
+  };
+  services.dnscrypt-proxy = {
+    enable = true;
+    settings = {
+      listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+
+      max_clients = 250;
+
+      dnscrypt_servers = false;
+      doh_servers = true;
+      odoh_servers = false;
+      ipv4_servers = true;
+      ipv6_servers = false;
+
+      require_dnssec = true;
+      require_nolog = true;
+      require_nofilter = true;
+
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+    };
+  };
+  services.resolved = {
+    enable = true;
+    settings.Resolve = {
+      FallbackDNS = "127.0.0.1 ::1";
+      LLMNR = false;
+    };
   };
 
   # flatpak
